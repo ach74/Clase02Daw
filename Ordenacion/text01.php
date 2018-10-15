1ce38ccf618f51ee0f379e9d57d98b1bded70e99
ach74 <32370243+ach74@users.noreply.github.com>
2018-10-15 14:52:26 +0200

    ach

--- Ordenacion/text01.php
+++ Ordenacion/text01.php
@@ -1,14 +1,45 @@
 <!DOCTYPE html>
 <html>
 <body>
+
+
 	<?php
 
+	/**
+	 * Programa que ordena una array
+	 *@author Achraf El Ouakili
+	 * 
+	 */ 
+
+	/**
+	 * Variables recofidas del form
+	 * @var $opSelect1,$opSelect2,$valoresTeclado,$tamaño integer 
+	 */
+	$tamaño = $_POST["tamañoArray"];
+	$valoresTeclado =  $_POST["valoresTeclado"];
+	$opSelect1 = $_POST["select1"];
+	$opSelect2 = $_POST["select2"];
+
+	/**
+	 * Inicio del programa
+	 * @method void main
+	 */
+	main();
+	
 	/**
 	 * Nos genera una array desordenada dependiendo del metodo  de ordenacion elegido
 	 * @return array
 	 */
 	function tipoArray(){
+		/**
+		 * Datos de caracte groval, para poder ser usados en esta funcion
+		 * @global $opSelect2,$valoresTeclado,$tamaño integer
+		 * 
+		 */
+
 		global $opSelect2,$valoresTeclado,$tamaño;
+
+		//Condicionales "if" para elegir el metodo que genera la array desordenada
 		if ($opSelect2==1) {
 			echo "<br> Aleatoria  <br>";
 			$arrayAleatoria = array();
@@ -35,8 +66,15 @@
 	 * @return type $array 
 	 */
 	function burbuja($array){
+
+		// Se necesitan dos bucles for anindados con un  limite de las posiciones posibles de la array
 		for($i=1;$i<count($array);$i++){
 			for($j=0;$j<count($array)-$i;$j++){
+				/**
+				 * Condicion "if" donde intecambiamos posiciones 
+				 * si el elemtento dentro de la array es superior 
+				 * al siguiente elemnto de la array.
+				 */
 				if($array[$j]>$array[$j+1]){
 					$a=$array[$j+1];
 					$array[$j+1]=$array[$j];
@@ -49,17 +87,47 @@
 
 	/**
 	 * Metodo de ordenacion por selecion directa
-	 * @param type $array 
-	 * @return type
+	 * @param array $array 
+	 * @return array $array 
 	 */
 	function selecion($array){
+
+		/**
+		 * Dos bucles "for" anidados para recorrer y ordenar la array, 
+		 * donde el limite del primer bucle es la posicion de la array - 1
+		 */ 
 		for ($i=0; $i<(count($array)-1); $i++){
+
+			/**
+			 * Variable para almacenar el valor minimo actual
+			 * @var $min integer
+			 */ 
+
 			$min=$i;
+
+			/**
+			 * Segundo bucle "for" donde el contador inicia con 
+			 * el valor del segundo bucle mas 1, asi iremos rellenando
+			 * el aregle de izquierda a derecha empezando por el valor 
+			 * mas pequeño.
+			 */ 
 			for($j=$i+1; $j<count($array); $j++){
+				/**
+				 * Condicion "if" que iguala la variable $min con $j,
+				 * en el caso de que el valor contenido en "$array[$j]"
+				 * sea inferior a "$array[$min]"
+				 */ 
 				if($array[$min] > $array[$j]){
 					$min=$j;
 				}
-			}$aux=$array[$min];
+			}
+
+			/**
+			 * Intercambio de posiciones de variables 
+			 * donde el valor contenido en "$array[$min]"
+			 * sera posicionado en el primer lugar del array
+			 */ 
+			$aux=$array[$min];
 			$array[$min]=$array[$i];
 			$array[$i]=$aux;
 		}
@@ -72,8 +140,19 @@
 	 * @return type
 	 */
 	function directa($array){
+		/**
+		 * Bucle "for" con la condicion de recorrer
+		 * todas la posiciones de la array, la variable 
+		 * generada en bucle inicia en 1
+		 */ 
 		for ($i=1; $i<(count($array)); $i++){
+
+			//Variable auxiliar que almacena el valor de coresponditene a la array en al posicion "$i"
 			$aux=$array[$i];
+
+			/**
+			 * Segundo bucle 
+			 */ 
 			for($j=$i-1;$j>=0 && $array[$j]>$aux; $j--){
 				$array[$j+1]=$array[$j];
 				$array[$j]=$aux;
@@ -82,61 +161,63 @@
 		return $array;
 	}
 
+	/**
+	 * Metodo main donde inicia la ejecucion del programa
+	 * @return void
+	 */
+	function main(){
 
-	//Hacer main
+		global $opSelect1;
 
-	$tamaño = $_POST["tamañoArray"];
-	$valoresTeclado =  $_POST["valoresTeclado"];
-	$opSelect1 = $_POST["select1"];
-	$opSelect2 = $_POST["select2"];
+		if ($opSelect1==1) {
 
-	if ($opSelect1==1) {
+			echo "<br><br> <h2>Burbuja </h2><br>";	
 
-		echo "<br><br> <h2>Burbuja </h2><br>";	
+			$arrayDesordenada = tipoArray();
 
-		$arrayDesordenada = tipoArray();
+			echo "<hr> Array desordenada : <br> <br> ";
 
-		echo "<hr> Array desordenada : <br> <br> ";
+			print_r($arrayDesordenada);
 
-		print_r($arrayDesordenada);
+			echo "<hr> Array ordenada : <br> <br>";
 
-		echo "<hr> Array ordenada : <br> <br>";
+			$arrayOrdenada = burbuja($arrayDesordenada);
 
-		$arrayOrdenada = burbuja($arrayDesordenada);
+			print_r ($arrayOrdenada);
 
-		print_r ($arrayOrdenada);
+		}elseif ($opSelect1==2) {
+			echo "<br><br> <h2>Inserción Directa </h2><br>";	
 
-	}elseif ($opSelect1==2) {
-		echo "<br><br> <h2>Inserción Directa </h2><br>";	
+			$arrayDesordenada = tipoArray();
 
-		$arrayDesordenada = tipoArray();
+			echo "<hr> Array desordenada : <br> <br> ";
 
-		echo "<hr> Array desordenada : <br> <br> ";
+			print_r($arrayDesordenada);
 
-		print_r($arrayDesordenada);
+			echo "<hr> Array ordenada : <br> <br>";
 
-		echo "<hr> Array ordenada : <br> <br>";
+			$arrayOrdenada = directa($arrayDesordenada);
 
-		$arrayOrdenada = directa($arrayDesordenada);
+			print_r ($arrayOrdenada);
+		}elseif ($opSelect1==3) {
 
-		print_r ($arrayOrdenada);
-	}elseif ($opSelect1==3) {
+			echo "<br><br> <h2>Selección directa </h2><br>";	
 
-		echo "<br><br> <h2>Selección directa </h2><br>";	
+			$arrayDesordenada = tipoArray();
 
-		$arrayDesordenada = tipoArray();
+			echo "<hr> Array desordenada : <br> <br> ";
 
-		echo "<hr> Array desordenada : <br> <br> ";
+			print_r($arrayDesordenada);
 
-		print_r($arrayDesordenada);
+			echo "<hr> Array ordenada : <br> <br>";
 
-		echo "<hr> Array ordenada : <br> <br>";
+			$arrayOrdenada = selecion($arrayDesordenada);
 
-		$arrayOrdenada = selecion($arrayDesordenada);
-
-		print_r ($arrayOrdenada);
+			print_r ($arrayOrdenada);
+		}
 	}
-	
+
+
 
 	?>
 </body>
